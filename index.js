c54f533c866c2d30c3d359769a0003340cbb5fb3
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-14 18:10:24 +0530

    uploading of blog

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const path = require("path");
 const mongoose = require("mongoose");
 
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const Blog = require("./models/blog");
 
 const app = express();
 const PORT = 8000;
@@ -18,13 +21,17 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get('/', (req, res)=>{
+app.get('/', async (req, res)=>{
+    const allBlogs = await Blog.find({});
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 })
 
 app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 app.listen(PORT, ()=>{console.log(`Server started at Port: ${PORT}`);})
