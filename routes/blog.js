8407d0ac219ac6286330e11660d6b3831382d288
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-14 19:55:54 +0530

    Post blog feature added

--- routes/blog.js
+++ routes/blog.js
@@ -22,6 +22,15 @@ router.get('/add-new', (req, res) => {
         user: req.user,
     })
 })
+
+router.get('/:id', async (req, res)=>{
+    const blog = await Blog.findById(req.params.id);
+    return res.render("blog", {
+        user: req.user,
+        blog,
+    })
+})
+
 router.post('/', upload.single('coverImage'), async (req, res) => {
     // console.log(req.body);
     // console.log(req.file);
