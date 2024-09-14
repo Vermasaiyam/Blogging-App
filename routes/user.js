ba5e650b33c6c1163a46a5a94b72786ebc6ae181
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-14 22:18:00 +0530

    profile photo

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,21 @@
 const { Router } = require("express");
+const multer = require("multer");
+const path = require("path");
 const User = require("../models/user");
 
 const router = Router();
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.resolve(`./public/images/`))
+//     },
+//     filename: function (req, file, cb) {
+//         const fileName = `${Date.now()}-${file.originalname}`;
+//         cb(null, fileName);
+//     }
+// })
+
+// const upload = multer({ storage: storage });
 
 router.get('/signin', (req, res) => {
     return res.render('signin');
@@ -26,11 +40,14 @@ router.post('/signin', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
     const { fullName, email, password } = req.body;
+    // console.log(req.file);
+    
 
     await User.create({
         fullName,
         email,
         password,
+        // profileImageUrl: `/images/${req.file.filename}`,
     })
     res.redirect('/user/signin');
 })
